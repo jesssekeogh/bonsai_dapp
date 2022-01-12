800cb19100b52dd1110129d2ce1fb9f79eb2b5e5
Jesse Keogh <jessekeogh@jesses-air.home>
2022-01-12 14:53:40 +0000

    Initial commit.

---
+++ src/kontribute_dapp_assets/src/index.js
@@ -0,0 +1,19 @@
+import { kontribute_dapp } from "../../declarations/kontribute_dapp";
+
+document.querySelector("form").addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const button = e.target.querySelector("button");
+
+  const name = document.getElementById("name").value.toString();
+
+  button.setAttribute("disabled", true);
+
+  // Interact with foo actor, calling the greet method
+  const greeting = await kontribute_dapp.greet(name);
+
+  button.removeAttribute("disabled");
+
+  document.getElementById("greeting").innerText = greeting;
+
+  return false;
+});
