07ebd8feb883fb3ad3e00c97fc6064f1a4d1839f
Jesse Keogh <jessekeogh@jesses-air.home>
2022-02-14 22:39:14 +0000

    Nav css, image css

--- src/kontribute_dapp_assets/src/Stories.jsx
+++ src/kontribute_dapp_assets/src/Stories.jsx
@@ -1,30 +1,32 @@
-import React from 'react';
+import React from "react";
 import "../assets/main.css";
-import {NavBar} from './containers';
-import {Link} from 'react-router-dom';
+import bonsai_bg from "../assets/beauty_render3_5.png";
+import { NavBar } from "./containers";
+import { Link } from "react-router-dom";
 import { Slide } from "react-awesome-reveal";
+import { Image } from "@chakra-ui/react";
 
 // stories page will link in our stories here
 const Stories = () => {
-
-    return (
-        <div>
-            <NavBar />
-             <Slide>
-            <div className="bonsai__card">
-            <div className="bonsai__card-content">
+  return (
+    <div>
+      <NavBar />
+      <Slide>
+        <div className="bonsai__card">
+            <Image className="bonsai__image" src={bonsai_bg} />
+          <div className="bonsai__card-content">
                 <p>Chapter 1</p>
                 <h3>The World of Bonsai</h3>
             </div>
-                <div className="bonsai__card-btn">
+          <div className="bonsai__card-btn">
                     <Link to="/world-of-bonsai">
                     <button type="button">Read Now</button>
                     </Link>
                 </div>
-            </div>
-            </Slide>
         </div>
-    )
-}
+      </Slide>
+    </div>
+  );
+};
 
-export default Stories
+export default Stories;
