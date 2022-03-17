c7915434d311e3f02d0fe9371cdc752bf29cd9c9
Jesse Keogh <jessekeogh@jesses-air.home>
2022-03-17 01:17:54 +0000

    NFT page updates

--- src/kontribute_dapp_assets/src/pages/nft/NFT.jsx
+++ src/kontribute_dapp_assets/src/pages/nft/NFT.jsx
@@ -2,33 +2,89 @@ import React, { useState, useEffect } from "react";
 import { NavBar } from "../../containers";
 import anvillogo from "../../../assets/anvillogo.svg"; // get logo from site
 import React from "react";
-
-//images:
-import bonsai_1 from "../../../assets/bonsai_nfts/bonsai_1.png";
-import bonsai_2 from "../../../assets/bonsai_nfts/bonsai_2.png";
-import bonsai_3 from "../../../assets/bonsai_nfts/bonsai_3.png";
-import bonsai_4 from "../../../assets/bonsai_nfts/bonsai_4.png";
-import bonsai_5 from "../../../assets/bonsai_nfts/bonsai_5.png";
-
 import { Box, Stack, Heading, Text, Spinner } from "@chakra-ui/react";
 import { Stack, Heading, Text } from "@chakra-ui/react";
 import {
   Box,
   Center,
-  useColorModeValue,
   Heading,
   Text,
   Stack,
   Button,
-  Grid,
+  SimpleGrid,
   GridItem,
+  Wrap
 } from "@chakra-ui/react";
 import { Image as ChakraImage } from "@chakra-ui/react";
+
+// get all the images
+import IMAGES from '../../../assets/bonsai_nfts';
+// optimise loading
+import { LazyLoadComponent } from 'react-lazy-load-image-component';
 
 // stories page css for spinner
 import "../stories/stories.css";
 
+const GridComponent = (props) => {
+  return (
+    <GridItem>
+      {/* turn this into a component with names as props and the option to have a sold out button(true or false?) */}
+      <Box
+        role={"group"}
+        p={4}
+        maxW={"330px"}
+        w={"full"}
+        backgroundColor={"#1e212b"}
+        boxShadow={"2xl"}
+        rounded={"lg"}
+        pos={"relative"}
+      >
+        <Box rounded={"lg"} pos={"relative"}>
+          <ChakraImage
+            rounded={"lg"}
+            height={"300px"}
+            width={"auto"}
+            objectFit={"cover"}
+            src={props.imgsrc}
+          />
+        </Box>
+        <Stack pt={3} align={"start"}>
+          <Text
+            color={"gray.500"}
+            fontSize={{ base: "sm", sm: "xs", md: "md" }}
+          >
+            Bonsai Warrior
+          </Text>
+          </Stack>
+          <Stack pt={2} direction={"row"} align={"center"} justify="space-between">
+            <Heading
+              fontSize={{ base: "lg", sm: "md", md: "lg" }}
+              color={"white"}
+            >
+              {props.name}
+            </Heading>
+            <a 
+              href={props.anvillink} 
+              target="_blank"
+              rel="noreferrer">
+            <Button
+              maxW="120px"
+              rounded={"full"}
+              color={"white"}
+              bgGradient="linear(to-r, #c61682, #ee670d)"
+              _hover={{ opacity: "0.8" }}
+            >
+              Buy Now
+            </Button>
+            </a>
+        </Stack>
+      </Box>
+    </GridItem>
+  );
+};
+
 const NFT = () => {
+
   // optimise image loading
   const [imageIsReady, setIsReady] = useState(false);
 
@@ -37,7 +93,7 @@ const NFT = () => {
     img.onload = () => {
       setIsReady(true);
     };
-    img.src = bonsai_1;
+    img.src = IMAGES.bonsai_1;
     window.scrollTo(0, 0);
   }, []);
 
@@ -56,61 +112,22 @@ const NFT = () => {
         </div>
       ) : null}
       {imageIsReady ? (
-        <Grid
-          px={20}
-          h="200px"
-          templateRows="repeat(2, 1fr)"
-          templateColumns="repeat(5, 1fr)"
-          gap={4}
-        >
-          <GridItem>
-            {/* turn this into a component with names as props and the option to have a sold out button(true or false?) */}
-            <Box
-              role={"group"}
-              p={6}
-              maxW={"330px"}
-              w={"full"}
-              bg={useColorModeValue("white", "gray.800")}
-              boxShadow={"2xl"}
-              rounded={"lg"}
-              pos={"relative"}
-              zIndex={1}
-            >
-              <Box rounded={"lg"} mt={-12} pos={"relative"} height={"230px"}>
-                <ChakraImage
-                  rounded={"lg"}
-                  height={230}
-                  width={282}
-                  objectFit={"cover"}
-                  src={bonsai_1}
-                />
-              </Box>
-              <Stack pt={10} align={"center"}>
-                <Text
-                  color={"gray.500"}
-                  fontSize={"sm"}
-                  textTransform={"uppercase"}
-                >
-                  Bonsai Warrior
-                </Text>
-                <Heading fontSize={"2xl"} fontFamily={"body"} fontWeight={500}>
-                  Name here
-                </Heading>
-                <Stack direction={"row"} align={"center"}>
-                  <Button
-                    rounded={"full"}
-                    color={"white"}
-                    flex={"1 0 auto"}
-                    bgGradient="linear(to-r, #c61682, #ee670d)"
-                    _hover={{ opacity: "0.8" }}
-                  >
-                    Buy Now
-                  </Button>
-                </Stack>
-              </Stack>
-            </Box>
-          </GridItem>
-        </Grid>
+        <Center>
+          {/* add in lazy load here test when deployed but remove nft link */}
+        <SimpleGrid columns={[1, null, 4]} pb={5} px={10} gap={4} maxW="1500px">
+          <GridComponent name="Demon General" imgsrc={IMAGES.bonsai_1} anvillink={}/>
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_2} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_3} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_4} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_5} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_6} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_7} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_8} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_9} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_10} />
+          <GridComponent name="the name" imgsrc={IMAGES.bonsai_11} />
+        </SimpleGrid>
+        </Center>
       ) : null}
     </div>
   );
